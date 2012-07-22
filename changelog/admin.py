9000c7286a9a5323fb0b760fbc402c867f46b397
Anubisss <david.vas1@gmail.com>
2012-07-22 02:32:55 +0200

    Kis fejlesztes az admin feluleten.
    Signed-off-by: Anubisss <david.vas1@gmail.com>

--- changelog/admin.py
+++ changelog/admin.py
@@ -1,6 +1,18 @@
 from changelog.models import ChangelogEntry, ChangelogLabel, ChangelogLabelEntry
 from django.contrib import admin
 
-admin.site.register(ChangelogEntry)
-admin.site.register(ChangelogLabel)
-admin.site.register(ChangelogLabelEntry)
+class ChangelogLabelEntryInline(admin.TabularInline):
+    model = ChangelogLabelEntry
+    extra = 3 # alapbol 3 ures, kitoltheto ChangelogLabelEntry megjelenik
+
+class ChangelogEntryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_str') # listazasnal ezek a mezok jelennek meg
+    list_display_links = ('id', 'date_str') # mindket oszlop legyen "kattinthato"
+    inlines = [ChangelogLabelEntryInline] # ugyanitt listazzuk/szerkesztjuk a ChangelogLabelEntry-ket is
+
+class ChangelogLabelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_editable = ('name',) # mar a listazasnal szerkeszthetok a nevek
+
+admin.site.register(ChangelogEntry, ChangelogEntryAdmin)
+admin.site.register(ChangelogLabel, ChangelogLabelAdmin)
--- changelog/models.py
+++ changelog/models.py
@@ -4,6 +4,14 @@ from django.db import models
 class ChangelogEntry(models.Model):
     date = models.DateField() # a changelog bejegyzes "cime"
 
+    def __unicode__(self):
+        return self.date.__str__()
+
+    # "szep" abrazolasa a datumnak
+    def date_str(self):
+        return self.date.strftime('%Y. %m. %d.')
+    date_str.short_description = 'Date' # ez a nev jelenik meg az admin feluleten az oszlopnal
+
     class Meta:
         db_table = 'changelog_entry' # table nev felulirasa, changelog_changelogentry helyett
 
@@ -12,8 +20,12 @@ class ChangelogEntry(models.Model):
 class ChangelogLabel(models.Model):
     name = models.CharField(max_length=64, unique=True) # a label neve, pl.: Egyeb, Event, Druid
 
+    def __unicode__(self):
+        return self.name
+
     class Meta:
         db_table = 'changelog_label'
+        ordering = ['name'] # rendezes nev alapjan, ABC sorrend
 
 # changelog kategoriahoz bejegyzes
 # lista elem es a ChangelogLabel a lista "cime"
@@ -21,6 +33,9 @@ class ChangelogLabelEntry(models.Model):
     content = models.CharField(max_length=512) # valodi tartalom, pl.: x spell javitva
     changelog_entry_id = models.ForeignKey(ChangelogEntry, db_column='changelog_entry_id') # a bejegyzes melyik changeloghoz tartozik
     changelog_label_id = models.ForeignKey(ChangelogLabel, db_column='changelog_label_id') # a bejegyzes melyik kategoriahoz tartozik
+
+    def __unicode__(self):
+        return self.content
 
     class Meta:
         db_table = 'changelog_label_entry'
