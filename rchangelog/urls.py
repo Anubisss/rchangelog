21a6620fb719e2ea64587e8d109dd3369b9b31b6
Anubisss <david.vas1@gmail.com>
2012-08-04 17:50:57 +0200

    A regi URL-eket is szolgaljuk ki.
    Signed-off-by: Anubisss <david.vas1@gmail.com>

--- rchangelog/urls.py
+++ rchangelog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     url(r'^(?P<year>\d{4})/$', 'changelog.views.archive_year'),
     url(r'^(?P<year>\d{4})/(?P<month>0[1-9]|1[0-2])/$', 'changelog.views.archive_month'),
     url(r'^(?P<year>\d{4})/(?P<month>0[1-9]|1[0-2])/(?P<day>0[1-9]|[12][0-9]|3[0-1])/$', 'changelog.views.detail'),
+    url(r'^(?P<year>\d{4})_(?P<month>0[1-9]|1[0-2])_(?P<day>0[1-9]|[12][0-9]|3[0-1]).html$', 'changelog.views.detail'), # regi tipusu URL-ek kiszolgalasa, 2011_03_02.html
     url(r'^nagyonkiralycsavok/', include(admin.site.urls)),
 )
