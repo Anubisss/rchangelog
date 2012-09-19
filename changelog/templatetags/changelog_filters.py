5da6ee66871fe22066735843ea5d41dfe5872fda
Anubisss <anubisss210@gmail.com>
2012-09-20 00:23:53 +0200

    Custom template filterek/tagek mergelese 2 fajlba.
    Signed-off-by: Anubisss <anubisss210@gmail.com>

--- changelog/templatetags/changelog_filters.py
+++ changelog/templatetags/changelog_filters.py
@@ -1,11 +1,10 @@
-# based on https://code.djangoproject.com/wiki/CookBookTemplateFilterBBCode
-
-import re
 from django import template
+import re
 
 register = template.Library()
 
 # egyszeru bbcode
+# based on https://code.djangoproject.com/wiki/CookBookTemplateFilterBBCode
 @register.filter
 def bbcode(value):
     bbdata = [
@@ -22,3 +21,13 @@ def bbcode(value):
         value = p.sub(bbset[1], value)
 
     return value
+
+# elso paragrafussal (<p>paragrafus</p>) visszateres
+# ha egy sincs, akkor visszateres a teljes szoveggel
+@register.filter
+def first_p(value):
+    m = re.search(r'^(<p>.+</p>)$', value, flags=re.MULTILINE)
+    if m == None: # nincs talalat
+        return value
+    p = m.group(1)
+    return p
--- changelog/templatetags/changelog_tags.py
+++ changelog/templatetags/changelog_tags.py
@@ -2,7 +2,6 @@ from django import template
 
 register = template.Library()
 
-# TODO: a 2 tag mergelese tags.py-be
 @register.simple_tag
 def active_navbar(req_path, url_to_match):
     if req_path == url_to_match:
