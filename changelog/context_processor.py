313a50b9f4a318d1e9d09b57c1d1c1b1e0636439
Anubisss <anubisss210@gmail.com>
2012-09-22 02:33:26 +0200

    Menuben megjeleno utolso bejegyzes, most mar csak publikusat mutat.
    Signed-off-by: Anubisss <anubisss210@gmail.com>

--- changelog/context_processor.py
+++ changelog/context_processor.py
@@ -1,8 +1,11 @@
 from changelog.models import ChangelogEntry
 from django.conf import settings
 
+# sajat context processor
+# segitsegevel megoldhato, hogy ezek a parameterek minden egyes template-nek
+# atadodjanak ha a template render-nel van context instance megadva
 def my_context_processor(request):
-    latest_changelog = ChangelogEntry.objects.latest(field_name='date') # TODO: public
+    latest_changelog = ChangelogEntry.objects.filter(public=True).latest()
     return {
         'SITE_NAME': settings.SITE_NAME,
         'SITE_FOOTER_ADDRESS': settings.SITE_FOOTER_ADDRESS,
